cc97ab4c8ec4248e89e803b8d52a15697e948231
Eduardo Felizardo <edufelizardo1@gmail.com>
2023-02-08 18:24:07 -0300

    paciente one

--- saude/src/main/java/com/mais/saude/models/pacientes/Paciente.java
+++ saude/src/main/java/com/mais/saude/models/pacientes/Paciente.java
@@ -1,8 +1,58 @@
 package com.mais.saude.models.pacientes;
 
 import com.mais.saude.models.Pessoa;
-import com.mais.saude.models.funcionarios.Equipe;
 
 public class Paciente extends Pessoa {
-    private Equipe equipe;
+    private Boolean pacienteArea;
+    private int area;
+    private int microArea;
+    private int familia;
+    private int membro;
+
+
+    /**********************Construtores**************************/
+
+
+    /*************************Metodos****************************/
+
+
+    /*************************Geters*****************************/
+    public Boolean getPacienteArea() {
+        return pacienteArea;
+    }
+    public int getArea() {
+        return area;
+    }
+    public int getMicroArea() {
+        return microArea;
+    }
+    public int getFamilia() {
+        return familia;
+    }
+    public int getMembro() {
+        return membro;
+    }
+
+    /*************************Seters*****************************/ 
+    public void setPacienteArea(Boolean pacienteArea) {
+        this.pacienteArea = pacienteArea;
+    }
+    public void setArea(int area) {
+        this.area = area;
+    }
+    public void setMicroArea(int microArea) {
+        this.microArea = microArea;
+    }
+    public void setFamilia(int familia) {
+        this.familia = familia;
+    }
+    public void setMembro(int membro) {
+        this.membro = membro;
+    }
+
+    /*********************HashCode/Equals************************/
+
+
+    /*************************output*****************************/
+
 }
